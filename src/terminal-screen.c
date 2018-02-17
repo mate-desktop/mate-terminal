53b470dac42b6c91f965b38375922629e3395223
monsta <monsta@inbox.ru>
2018-02-17 17:34:25 +0300

    screen: don't set TERM environment variable once again
    This reverts commit 8e3de226d0c59060bd06aa0010fdc6ef1e37e0ca.

--- src/terminal-screen.c
+++ src/terminal-screen.c
@@ -1421,8 +1421,6 @@ get_child_environment (TerminalScreen *screen,
 	g_hash_table_remove (env_table, "LINES");
 	g_hash_table_remove (env_table, "MATE_DESKTOP_ICON");
 
-	g_hash_table_replace (env_table, g_strdup ("TERM"), g_strdup ("xterm")); /* FIXME configurable later? */
-
 	/* FIXME: moving the tab between windows, or the window between displays will make the next two invalid... */
 	g_hash_table_replace (env_table, g_strdup ("WINDOWID"), g_strdup_printf ("%ld", GDK_WINDOW_XID (gtk_widget_get_window (window))));
 	g_hash_table_replace (env_table, g_strdup ("DISPLAY"), g_strdup (gdk_display_get_name (display)));
