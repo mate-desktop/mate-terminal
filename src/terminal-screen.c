846ee3941d39c50f6c3baf1cf6985a8bf808c445
Wu Xiaotian <yetist@gmail.com>
2020-06-20 20:18:10 +0000

    Compile code only when GDK supports X11

--- src/terminal-screen.c
+++ src/terminal-screen.c
@@ -1445,9 +1445,11 @@ get_child_environment (TerminalScreen *screen,
 	g_hash_table_replace (env_table, g_strdup ("TERM"), g_strdup ("xterm-256color")); /* FIXME configurable later? */
 
 	/* FIXME: moving the tab between windows, or the window between displays will make the next two invalid... */
+#ifdef GDK_WINDOWING_X11
     if (GDK_IS_X11_DISPLAY (display)) {
         g_hash_table_replace (env_table, g_strdup ("WINDOWID"), g_strdup_printf ("%ld", GDK_WINDOW_XID (gtk_widget_get_window (window))));
     }
+#endif
 	g_hash_table_replace (env_table, g_strdup ("DISPLAY"), g_strdup (gdk_display_get_name (display)));
 
 	g_settings_schema_source_list_schemas (g_settings_schema_source_get_default (), TRUE, &list_schemas, NULL);
