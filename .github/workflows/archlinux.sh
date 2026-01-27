9b7e3c7279cdfc6ad6597d858230d8fda315b29d
mbkma <johannes.unruh@fau.de>
2026-01-27 23:12:50 +0100

    ci: migrate from Travis CI to GitHub Actions
    Replace Travis CI configuration with GitHub Actions workflows.
    Add CI build workflow for Debian, Fedora, Ubuntu, and Archlinux.
    Add release workflow for automated GitHub releases on tags.
    Add dependabot configuration for GHA pin updates.
    Remove obsolete .travis.yml.

---
+++ .github/workflows/archlinux.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/bash
+
+# Use grouped output messages
+infobegin() {
+	echo "::group::${1}"
+}
+infoend() {
+	echo "::endgroup::"
+}
+
+# Required packages on Archlinux
+requires=(
+	ccache # Use ccache to speed up build
+	meson  # Used for meson build
+)
+
+requires+=(
+	autoconf-archive
+	gcc
+	git
+	libsm
+	make
+	mate-common
+	mate-desktop
+	vte3
+	which
+	yelp-tools
+)
+
+infobegin "Update system"
+pacman --noconfirm -Syu
+infoend
+
+infobegin "Install dependency packages"
+pacman --noconfirm -S ${requires[@]}
+infoend
---
+++ .github/workflows/fedora.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/bash
+
+# Use grouped output messages
+infobegin() {
+	echo "::group::${1}"
+}
+infoend() {
+	echo "::endgroup::"
+}
+
+# Required packages on Fedora
+requires=(
+	ccache # Use ccache to speed up build
+	meson  # Used for meson build
+)
+
+requires+=(
+	autoconf-archive
+	gcc
+	dconf-devel
+	desktop-file-utils
+	git
+	gtk3-devel
+	libSM-devel
+	make
+	mate-common
+	redhat-rpm-config
+	vte291-devel
+)
+
+infobegin "Update system"
+dnf update -y
+infoend
+
+infobegin "Install dependency packages"
+dnf install -y ${requires[@]}
+infoend
